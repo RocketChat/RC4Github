80ad33b1e4728ceb12b4f2eee4576e911f15ed98
Dhruv Jain <dhruv.jain93@gmail.com>
2021-01-20 18:45:23 +0530

    [NEW] User model

---
+++ server/models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    avatarUrl: {
+        type: String,
+        required: true
+    },
+    rcPassword: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+const user = mongoose.model('User', userSchema)
+module.exports = user

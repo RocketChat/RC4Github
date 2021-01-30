ee55abe489ad742c2547545517c8d8a18b7fff5c
Dhruv Jain <dhruv.jain93@gmail.com>
2021-01-30 14:34:47 +0530

    [FEATURE] Iframe Login support using SSO

--- server/app.js
+++ server/app.js
@@ -7,8 +7,18 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const port = process.env.PORT || 3030
+const whitelist = ["http://localhost:3000", "http://localhost:3002"];
+const corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true, credentials: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
+app.use(cors(corsOptionsDelegate));
 
-app.use(cors())
 app.use(bodyParser.json())
 app.use(passport.initialize())
 

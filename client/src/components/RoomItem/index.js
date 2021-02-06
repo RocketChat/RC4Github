4a1047d9a5d980d37113eae2f23c5ef8b0c4ed6e
Dhruv Jain <dhruv.jain93@gmail.com>
2021-02-06 19:50:26 +0530

    [FIX] Display DM room name without _rc4git suffix, Change room-type-icons

--- client/src/components/RoomItem/index.js
+++ client/src/components/RoomItem/index.js
@@ -1,31 +1,47 @@
 import { CgHashtag } from "react-icons/cg";
-import {BsFillLockFill} from "react-icons/bs";
+import {FiLock, FiUser} from "react-icons/fi";
 import { NavLink } from "react-router-dom";
 
 import "./index.css";
 import { rcApiDomain } from "../../utils/constants";
-export default function RoomItem({room}) {
-    return (
-      <NavLink
-        to={`/${room["t"] === 'c' ? "channel" : room["t"] === 'p' ? "group" : "direct"}/${room.name}`}
-        className="room-wrapper"
-        activeClassName="active-room"
-      >
-        <img
-          alt={"room-icon"}
-          src={`${rcApiDomain}/avatar/room/${room.rid}`}
-          className="room-item-icon"
-        ></img>
-        {room["t"] === "c" ? (
-          <CgHashtag className="room-item-type-icon"></CgHashtag>
-        ) : room["t"] === "p" ? (
-          <BsFillLockFill className="room-item-type-icon"></BsFillLockFill>
-        ) : null}
-        <span className="room-name">
-          {room.name.split(/_(.+)/)[1]
-            ? room.name.split(/_(.+)/)[1]
-            : room.name.split(/_(.+)/)[0]}
-        </span>
-      </NavLink>
-    );
+export default function RoomItem({ room }) {
+  return (
+    <NavLink
+      to={`/${
+        room["t"] === "c" ? "channel" : room["t"] === "p" ? "group" : "direct"
+      }/${room.username || room.name}`}
+      className="room-wrapper"
+      activeClassName="active-room"
+    >
+      <img
+        alt={"room-icon"}
+        src={`${rcApiDomain}/avatar/${
+          room["username"] || `room/${room.rid || room._id}`
+        }`}
+        className="room-item-icon"
+      ></img>
+      {room["t"] === "c" ? (
+        <CgHashtag className="room-item-type-icon"></CgHashtag>
+      ) : room["t"] === "p" ? (
+        <FiLock className="room-item-type-icon"></FiLock>
+      ) : (
+        <FiUser className="room-item-type-icon"></FiUser>
+      )}
+      <span className="room-name">
+        {room.name.split(/_(.+)/)[1] && room["t"] !== "d"
+          ? room.name.split(/_(.+)/)[1]
+          : room.name.substring(0, room.name.lastIndexOf("_rc4git")) === ""
+          ? room.name
+          : room.name.substring(0, room.name.lastIndexOf("_rc4git"))}
+        {room.username
+          ? ` (${
+              room.username.substring(
+                0,
+                room.username.lastIndexOf("_rc4git")
+              ) || room.username
+            })`
+          : null}
+      </span>
+    </NavLink>
+  );
 }

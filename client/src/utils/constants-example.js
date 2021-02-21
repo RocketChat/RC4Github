20d37d70f425fd6258df14cfee971ef0d78a6405
Rohan Lekhwani <rohanlekhwani@ymail.com>
2021-02-21 10:08:06 +0530

    [FIX] Changes default values for constants

--- client/src/utils/constants-example.js
+++ client/src/utils/constants-example.js
@@ -3,11 +3,11 @@ const githubPrivateRepoAccessClientID =
   process.env.GITHUB_PRIVATE_REPO_ACCESS_CLIENT_ID ||
   "YOUR_GITHUB_PRIVATE_REPO_ACCESS_CLIENT_ID";
 const rcApiDomain =
-  process.env.RC_API_DOMAIN || "https://omni2.rceng.shop/";
+  process.env.RC_API_DOMAIN || "YOUR_SELF_HOSTED_RC_API_DOMAIN";
 const githubApiDomain =
   process.env.GITHUB_API_DOMAIN || "https://api.github.com";
 const rc4gitDomain =
-  process.env.RC4GIT_DOMAIN || "https://rc4git.rceng.shop"
+  process.env.RC4GIT_DOMAIN || "YOUR_SELF_HOSTED_RC4GIT_CLIENT"
 
 export {
   githubClientID,

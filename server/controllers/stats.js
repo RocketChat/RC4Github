a8d37161c717ec7b08d3bc31305e4af58cf39806
Dhruv Jain <dhruv.jain93@gmail.com>
2021-02-28 13:10:10 +0530

    [NEW] Fetch Stats API

---
+++ server/controllers/stats.js
@@ -0,0 +1,25 @@
+const { rocketChatDomain, rc_uid, rc_token } = require("../config/constants");
+const axios = require('axios');
+module.exports.fetchStats = async (req, res) => {
+    try {
+        const RCStats = await axios({
+          method: "get",
+          url: `${rocketChatDomain}/api/v1/statistics?refresh=true`,
+          headers: {
+            "Content-type": "application/json",
+            "X-Auth-Token": `${rc_token}`,
+            "X-User-Id": `${rc_uid}`,
+          },
+        });
+        const stats = {
+          channels: RCStats.data.totalRooms,
+          users: RCStats.data.totalUsers,
+          onlineUsers: RCStats.data.onlineUsers,
+          totalMessages: RCStats.data.totalMessages
+        };
+        return res.status(200).json({success: true, data: stats});
+    } catch(err) {
+        console.log("ERROR --->", err);
+        return res.status(500).json({success: false, error: err});
+    }
+}

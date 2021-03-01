4041802e9a0f307d3d29c3f1fed2ba51272c068c
Dhruv Jain <dhruv.jain93@gmail.com>
2021-03-01 23:09:29 +0530

    [IMPROVEMENT] Handle count value greater than 1k

--- client/src/components/common/Countup.js
+++ client/src/components/common/Countup.js
@@ -1,14 +1,18 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 export default function Countup(props) {
   const [count, setCount] = useState(0);
+  const [value, setValue] = useState("");
   const speed = 1000 / props.end;
+  // eslint-disable-next-line
   useEffect(() => {
-    if (count < props.end) {
+    if (count < props.end && count < 1000) {
       setTimeout(() => {
         setCount((prevCount) => prevCount + 1);
       }, speed);
+    } else {
+      setValue(props.end >= 1000 ? `${props.end / 1000}k` : props.end);
     }
   });
-  return <div className={props.className}>{count}</div>;
+  return <div className={props.className}>{value || count}</div>;
 }
